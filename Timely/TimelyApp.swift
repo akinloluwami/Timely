d0cb8b52228a1470ae15a568c9fe1c64a123a45d
titanium-dev <akinkunmioye42@gmail.com>
2025-05-13 02:16:58 +0100

    Initial Commit

---
+++ Timely/TimelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelyApp.swift
+//  Timely
+//
+//  Created by Akinkunmi Oyewole on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TimelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimelyTests/TimelyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimelyTests.swift
+//  TimelyTests
+//
+//  Created by Akinkunmi Oyewole on 13/05/2025.
+//
+
+import Testing
+@testable import Timely
+
+struct TimelyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
